d45147837eafaac17da2b53bd976d8b3579ba72b
riyaanghosh <riyaanghosh@192.168.1.28>
2021-11-28 20:09:20 +0530

    updated design changes

--- src/My/MyTesting.java
+++ src/My/MyTesting.java
@@ -1,15 +1,31 @@
 package My;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MyTesting {
 	
 	public static void main(String arg[]) {
-		int a=10;
+		int a1=10;
 		int b=20;
-		int c=a+b;
-		System.out.println(a*b);
-		System.out.println(c);
+		int c=a1+b;
+
+		Integer[] a={58457907};
 		
-		System.out.println(a*b);
+		List<Integer> al = Arrays.asList(a);
+
+		Collections.sort(al);
+
+		int k = 4;
+
+		List<Integer> sl = al.subList(0, k);
+		
+		System.out.println(sl);
+		
+		int diff=sl.get(k-1)-sl.get(0);
+		
+		System.out.println(diff);
 	}
 
 }
